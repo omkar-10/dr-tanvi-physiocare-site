993534de4b7c30df8d2ac9ca5e8d41b1abedbf83
Omkar Dhavale <dhavale.omkar04@gmail.com>
2025-07-23 16:06:14 +0530

    Fix case-sensitive import for StatsSection

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,10 +2,9 @@
 import { motion } from "framer-motion";
 import { Link } from "react-router";
 import HeroSection from "../components/HeroSection";
-import StatsSection from "../components/statsSection";
+import StatsSection from "../components/StatsSection";
 import AboutDrTanvi from "../components/AboutDrTanvi";
 import ServicesSection from "../components/ServicesSection";
-import TestimonialsSection from "../components/TestimonialsSection";
 import ConditionsSection from "../components/ConditionsSection";
 import BookAppointmentCTA from "../components/BookAppointmentCTA";
 import GoogleReviewsSection from "../components/TestimonialsSection";
